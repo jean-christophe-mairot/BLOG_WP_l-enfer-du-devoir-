306278b6ce05100b688a548029e9aea5f5515cd9
jean christophe mairot <jcmairot@gmail.com>
2021-02-09 18:44:22 +0100

    change function .php

--- wp-content/themes/suit_enfant/functions.php
+++ wp-content/themes/suit_enfant/functions.php
@@ -2,17 +2,6 @@
 // /**
 // ** activation theme
 // **/
-// add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-// function theme_enqueue_styles() {
-//  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
-// }
-
-// add_action( 'wp_enqueue_scripts', 'theme_enqueue_script' );
-// function theme_enqueue_script() {
-//  wp_enqueue_script( 'parent-script', get_template_directory_uri() . '/js/suit.js' );
-
-// }
 
 function custom_enqueue_scripts() {
     //ajout pour js jquery
